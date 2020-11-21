7e61e8b93af54d720f2460a9a270f9383b6f978b
u_can_git_it <43105742+nivedita0701@users.noreply.github.com>
2020-11-22 02:54:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import h5py
 from keras.models import load_model
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 # load the dataset but only keep the top n words, zero the rest
 top_words = 10000
 max_words = 500
@@ -31,27 +32,119 @@ def pred(usermoviereview):
     sentiment = loaded_model.predict(x=review_tokens_pad)
     print(sentiment)
     if sentiment[0] > 0.5:
-        sentiment_str = "Our AI system thinks You like the movie and the percentage score of liking is about : " + "{0:.2%}".format(float(sentiment[0]))
+        sentiment_s = float(sentiment[0])
     else:
-        sentiment_str = "Our AI system thinks You didn't like the movie the percentage score of liking is about : " + "{0:.2%}".format(float(sentiment[0]))
-    return sentiment_str
+        sentiment_s = float(sentiment[0])
+    return sentiment_s
 
 # webapp
-app = Flask(__name__, template_folder='./')
+app = Flask(__name__, template_folder='./') 
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/my_data'
+
+db = SQLAlchemy(app)
+
 run_with_ngrok(app)
 
-@app.route('/')
+class shakuntala(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    review = db.Column(db.String(500))
+    percent = db.Column(db.Integer)
+    date_entered = db.Column(db.DateTime, default = datetime.now)
+
+class chhalaang(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    review = db.Column(db.String(500))
+    percent = db.Column(db.Integer)
+    date_entered = db.Column(db.DateTime, default = datetime.now)
+
+class harry(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    review = db.Column(db.String(500))
+    percent = db.Column(db.Integer)
+    date_entered = db.Column(db.DateTime, default = datetime.now)
+
+class avengers(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    review = db.Column(db.String(500))
+    percent = db.Column(db.Integer)
+    date_entered = db.Column(db.DateTime, default = datetime.now)
+
+class laxmii(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    review = db.Column(db.String(500))
+    percent = db.Column(db.Float)
+    date_entered = db.Column(db.DateTime, default = datetime.now)
+
+def shakuntala_rev(request):
+    sentiment_s = pred(request.form['message'])
+    rev = shakuntala(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
+    db.session.add(rev)
+    db.session.commit()
+
+
+def hp_rev(request):
+    sentiment_s = pred(request.form['message'])
+    rev = harry(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
+    db.session.add(rev)
+    db.session.commit()
+
+
+def avengers_rev(request):
+    sentiment_s = pred(request.form['message'])
+    rev = avengers(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
+    db.session.add(rev)
+    db.session.commit()
+
+
+def laxmii_rev(request):
+    sentiment_s = pred(request.form['message'])
+    rev = laxmii(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
+    db.session.add(rev)
+    db.session.commit()
+
+
+
+def chhalaang_rev(request):
+    sentiment_s = pred(request.form['message'])
+    rev = chhalaang(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
+    db.session.add(rev)
+    db.session.commit()
+
+
+@app.route('/', methods=['POST', 'GET'])
 def main():
+   
+    if request.method == "POST":
+        if 'sd' in request.form:
+            shakuntala_rev(request)
+            sdf = shakuntala.query.all()
+            return render_template("home.html", revs1=sdf)
+        if 'hp' in request.form:
+            hp_rev(request)
+            sdf = harry.query.all()
+            return render_template("home.html", revs2=sdf)
+        if 'av' in request.form:
+            avengers_rev(request)
+            sdf = avengers.query.all()
+            return render_template("home.html", revs3=sdf)
+        if 'ch' in request.form:
+            chhalaang_rev(request)
+            sdf = chhalaang.query.all()
+            return render_template("home.html", revs4=sdf)
+        if 'laxmii' in request.form:
+            laxmii_rev(request)
+            sdf = laxmii.query.all()
+            return render_template("home.html", revs5=sdf)
+        
     return render_template('home.html')
 
-@app.route('/process', methods=['POST', 'GET'])
-def process():
-    if request.method == "POST":
-        message = request.form['message']
-        print(message)
-        response =  pred(message)
-        print(type(response))
-        return jsonify(response=response)
 
 
 
