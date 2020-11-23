36a1571157364bc879294cdb38d2ccf7f62a526c
u_can_git_it <43105742+nivedita0701@users.noreply.github.com>
2020-11-24 02:22:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ import h5py
 from keras.models import load_model
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
+import math
+
+local_tz = pytz.timezone('Asia/Calcutta')
+
 # load the dataset but only keep the top n words, zero the rest
 top_words = 10000
 max_words = 500
@@ -16,6 +21,13 @@ max_words = 500
 #load the csv file saved
 
 df = pd.read_csv('./data/movie_data.csv', encoding='utf-8')
+def utc_to_local(utc_dt):
+    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)
+
+def aslocaltimestr(utc_dt):
+    return utc_to_local(utc_dt).strftime('%A %d %B %Y %H:%M:%S')
+
 
 tokenizer_obj = Tokenizer(num_words=top_words)
 tokenizer_obj.fit_on_texts(df.loc[:50000, 'review'].values)
@@ -41,7 +53,7 @@ def pred(usermoviereview):
 app = Flask(__name__, template_folder='./') 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/my_data'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 
@@ -52,38 +64,38 @@ class shakuntala(db.Model):
     name = db.Column(db.String(20))
     review = db.Column(db.String(500))
     percent = db.Column(db.Integer)
-    date_entered = db.Column(db.DateTime, default = datetime.now)
+    date_entered = db.Column(db.String, default=aslocaltimestr(datetime.now()))
 
 class chhalaang(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
     review = db.Column(db.String(500))
     percent = db.Column(db.Integer)
-    date_entered = db.Column(db.DateTime, default = datetime.now)
+    date_entered = db.Column(db.String, default=aslocaltimestr(datetime.now()))
 
 class harry(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
     review = db.Column(db.String(500))
     percent = db.Column(db.Integer)
-    date_entered = db.Column(db.DateTime, default = datetime.now)
+    date_entered = db.Column(db.String, default=aslocaltimestr(datetime.now()))
 
 class avengers(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
     review = db.Column(db.String(500))
     percent = db.Column(db.Integer)
-    date_entered = db.Column(db.DateTime, default = datetime.now)
+    date_entered = db.Column(db.String, default=aslocaltimestr(datetime.now()))
 
 class laxmii(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
     review = db.Column(db.String(500))
     percent = db.Column(db.Float)
-    date_entered = db.Column(db.DateTime, default = datetime.now)
+    date_entered = db.Column(db.String, default=aslocaltimestr(datetime.now()))
 
 def shakuntala_rev(request):
-    sentiment_s = pred(request.form['message'])
+    sentiment_s = pred(str(request.form['message']))
     rev = shakuntala(name=request.form['name'], review=request.form['message'], percent=sentiment_s)
     db.session.add(rev)
     db.session.commit()
@@ -118,32 +130,240 @@ def chhalaang_rev(request):
     db.session.commit()
 
 
+def truncate(number) -> float:
+    stepper = 10
+    return math.trunc(stepper * number) / stepper
+
 @app.route('/', methods=['POST', 'GET'])
 def main():
-   
+    
+    avg_sd = 0
+    avg_ch = 0
+    avg_hp = 0
+    avg_av = 0
+    avg_lx = 0
+    sdf = shakuntala.query.all()
+    count = 0
+    for q in sdf:
+        avg_sd += q.percent
+        count += 1
+    if count != 0:
+        avg_sd /= count
+        avg_sd *= 10
+
+    sdf2 = chhalaang.query.all()
+    count = 0
+    for q in sdf:
+        avg_ch += q.percent
+        count += 1
+    if count != 0:
+        avg_ch /= count
+        avg_ch *= 10
+
+    sdf3 = avengers.query.all()
+    count = 0
+    for q in sdf3:
+        avg_av += q.percent
+        count += 1
+    if count != 0:
+        avg_av /= count
+        avg_av *= 10
+
+    sdf4 = harry.query.all()
+    count = 0
+    for q in sdf4:
+        avg_hp += q.percent
+        count += 1
+    if count != 0:
+        avg_hp /= count
+        avg_hp *= 10
+
+    sdf5 = laxmii.query.all()
+    count = 0
+    for q in sdf5:
+        avg_lx += q.percent
+        count += 1
+    if count != 0:
+        avg_lx /= count
+        avg_lx *= 10
+    
+    avg_sd = truncate(avg_sd)
+    avg_ch = truncate(avg_ch)
+    avg_av = truncate(avg_av)
+    avg_hp = truncate(avg_hp)
+    avg_lx = truncate(avg_lx)
+
     if request.method == "POST":
+        sdf = shakuntala.query.all()
+        count = 0
+        avg_sd = 0
+        for q in sdf:
+            avg_sd += q.percent
+            count += 1
+        if count != 0:
+            avg_sd /= count
+            avg_sd *= 10
+
+        sdf2 = chhalaang.query.all()
+        count = 0
+        avg_ch = 0
+
+        for q in sdf:
+            avg_ch += q.percent
+            count += 1
+        if count != 0:
+            avg_ch /= count
+            avg_ch *= 10
+
+        sdf3 = avengers.query.all()
+        count = 0
+        avg_av = 0
+        for q in sdf3:
+            avg_av += q.percent
+            count += 1
+        if count != 0:
+            avg_av /= count
+            avg_av *= 10
+
+        sdf4 = harry.query.all()
+        count = 0
+        avg_hp = 0
+        for q in sdf4:
+            avg_hp += q.percent
+            count += 1
+        if count != 0:
+            avg_hp /= count
+            avg_hp *= 10
+
+        sdf5 = laxmii.query.all()
+        count = 0
+        avg_lx = 0
+        for q in sdf5:
+            avg_lx += q.percent
+            count += 1
+        if count != 0:
+            avg_lx /= count
+            avg_lx *= 10
+        
+        avg_sd = truncate(avg_sd)
+        avg_ch = truncate(avg_ch)
+        avg_av = truncate(avg_av)
+        avg_hp = truncate(avg_hp)
+        avg_lx = truncate(avg_lx)
+
         if 'sd' in request.form:
             shakuntala_rev(request)
+
             sdf = shakuntala.query.all()
-            return render_template("home.html", revs1=sdf)
+            count = 0
+            avg_sd = 0
+            for q in sdf:
+                avg_sd += q.percent
+                count += 1
+            if count != 0:
+                avg_sd /= count
+                avg_sd *= 10
+            sdf2 = chhalaang.query.all()
+            sdf3 = avengers.query.all()
+            sdf4 = harry.query.all()
+            sdf5 = laxmii.query.all()
+
+            avg_sd = truncate(avg_sd)
+            avg_ch = truncate(avg_ch)
+            avg_av = truncate(avg_av)
+            avg_hp = truncate(avg_hp)
+            avg_lx = truncate(avg_lx)
+            return render_template("home.html", revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5,avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx )
         if 'hp' in request.form:
             hp_rev(request)
-            sdf = harry.query.all()
-            return render_template("home.html", revs2=sdf)
+            sdf = shakuntala.query.all()
+            sdf2 = chhalaang.query.all()
+            sdf3 = avengers.query.all()
+            sdf4 = harry.query.all()
+            count = 0
+            avg_hp = 0
+            for q in sdf4:
+                avg_hp += q.percent
+                count += 1
+            if count != 0:
+                avg_hp /= count
+                avg_hp *= 10
+            sdf5 = laxmii.query.all()
+
+            avg_sd = truncate(avg_sd)
+            avg_ch = truncate(avg_ch)
+            avg_av = truncate(avg_av)
+            avg_hp = truncate(avg_hp)
+            avg_lx = truncate(avg_lx)
+            return render_template("home.html",revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5,avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx)
         if 'av' in request.form:
             avengers_rev(request)
-            sdf = avengers.query.all()
-            return render_template("home.html", revs3=sdf)
+            sdf = shakuntala.query.all()
+            sdf2 = chhalaang.query.all()
+            sdf3 = avengers.query.all()
+            count = 0
+            avg_av = 0
+            for q in sdf3:
+                avg_av += q.percent
+                count += 1
+            if count != 0:
+                avg_av /= count
+                avg_av *= 10
+            sdf4 = harry.query.all()
+            sdf5 = laxmii.query.all()
+
+            avg_sd = truncate(avg_sd)
+            avg_ch = truncate(avg_ch)
+            avg_av = truncate(avg_av)
+            avg_hp = truncate(avg_hp)
+            avg_lx = truncate(avg_lx)
+            return render_template("home.html", revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5,avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx)
         if 'ch' in request.form:
             chhalaang_rev(request)
-            sdf = chhalaang.query.all()
-            return render_template("home.html", revs4=sdf)
+            sdf = shakuntala.query.all()
+            sdf2 = chhalaang.query.all()
+            count = 0
+            avg_ch = 0
+            for q in sdf2:
+                avg_ch += q.percent
+                count += 1
+            if count != 0:
+                avg_ch /= count
+                avg_ch *= 10
+            sdf3 = avengers.query.all()
+            sdf4 = harry.query.all()
+            sdf5 = laxmii.query.all()
+
+            avg_sd = truncate(avg_sd)
+            avg_ch = truncate(avg_ch)
+            avg_av = truncate(avg_av)
+            avg_hp = truncate(avg_hp)
+            avg_lx = truncate(avg_lx)
+            return render_template("home.html", revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5,avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx)
         if 'laxmii' in request.form:
             laxmii_rev(request)
-            sdf = laxmii.query.all()
-            return render_template("home.html", revs5=sdf)
-        
-    return render_template('home.html')
+            sdf = shakuntala.query.all()
+            sdf2 = chhalaang.query.all()
+            sdf3 = avengers.query.all()
+            sdf4 = harry.query.all()
+            sdf5 = laxmii.query.all()
+            count = 0
+            avg_lx = 0
+            for q in sdf5:
+                avg_lx += q.percent
+                count += 1
+            if count != 0:
+                avg_lx /= count
+                avg_lx *= 10
+
+            avg_sd = truncate(avg_sd)
+            avg_ch = truncate(avg_ch)
+            avg_av = truncate(avg_av)
+            avg_hp = truncate(avg_hp)
+            avg_lx = truncate(avg_lx)
+            return render_template("home.html",revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5,avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx)
+    
+    return render_template('home.html',revs1=sdf, revs2=sdf2, revs3=sdf3, revs4=sdf4, revs5=sdf5, avg_sd=avg_sd, avg_av=avg_av, avg_ch=avg_ch, avg_hp=avg_hp, avg_lx=avg_lx)
 
 
 
